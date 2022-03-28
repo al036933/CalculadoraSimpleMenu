c6d37e1db3ba2402d55d29e997b8b4e2a7c7d28e
Cristian García Hernández <al036933@ieslaencanta.com>
2022-03-28 10:40:02 +0200

    Calculadora haciendo

---
+++ src/es/cristian/Main.java
@@ -0,0 +1,15 @@
+package es.cristian;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int numero;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("MENÚ");
+
+
+    }
+}
