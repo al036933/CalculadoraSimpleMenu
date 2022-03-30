9a5f706fe684deb7210e38b2a1dcf988ee9dd39e
Cristian García Hernández <al036933@ieslaencanta.com>
2022-03-30 09:47:57 +0200

    Calculadora haciendo

--- src/es/cristian/Main.java
+++ src/es/cristian/Main.java
@@ -5,11 +5,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    int numero;
+	    int opcion, n1 = 0, n2 = 0, resultado;
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("MENÚ");
+        System.out.println("=====================");
+        System.out.println("1.- Sumar. ");
+        System.out.println("2.- Restar. ");
+        System.out.println("0.- Salir. ");
+        System.out.print("Elige una opción: ");
+        opcion = entrada.nextInt();
 
+        if (opcion == 1) {
+            System.out.print("Primer sumando: ");
+            n1 = entrada.nextInt();
+            System.out.print("Segundo sumando: ");
+            n2 = entrada.nextInt();
+            resultado = n1+n2;
+            System.out.println("" + n1 + " + " + n2 + " = " + resultado);
+        } else if (opcion == 2) {
+            System.out.print("Primer minuendo: ");
+            n1 = entrada.nextInt();
+            System.out.print("Segundo sustraendo: ");
+            n2 = entrada.nextInt();
+            resultado = n1-n2;
+            System.out.println("" + n1 + " - " + n2 + " = " + resultado);
+        }
 
+        entrada.close();
     }
 }
